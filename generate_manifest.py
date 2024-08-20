5ee0318ad6b1c6b3930ea4fb318a82f0b9ac2c61
MeganKW <megan.k.w.12@gmail.com>
2024-08-20 13:36:32 -0700

    remove human setting packs and make no terminal gifs the default

--- generate_manifest.py
+++ generate_manifest.py
@@ -1,52 +1,14 @@
 import itertools
 import json
 
-PEOPLE = [
-    "ADAM_HANSON",
-    "AMRITANSHU_PARAD",
-    "AMY_NGO",
-    "BEN_WEST",
-    "BETH_BARNES",
-    "BRIAN_GOODRICH",
-    "CHRIS_BARNETT",
-    "CHRIS_MACLEOD",
-    "HAMISH_HUGGARD",
-    "DAVIS_ELLIS",
-    "JAI_DHYANI",
-    "JEFFREY_HAINES",
-    "JOEL_BECKER",
-    "JOSH_CLYMER",
-    "KATHERINE_WORDEN",
-    "KATHY_GARCIA",
-    "LAWRENCE_CHAN",
-    "LUCAS_SATO",
-    "MATT_GOLDWATER",
-    "MATTHEW_WEARDEN",
-    "MEGAN_KINNIMENT",
-    "MIGUEL_BRANDAO",
-    "MIHNEA_MAFTEI",
-    "MARTIN_MILBRADT",
-    "NIKOLA_JURKOVIC",
-    "NOA_WEISS",
-    "PIP_ARNOTT",
-    "RAE_SHE",
-    "RYAN_BLOOM",
-    "SAMI_JAWHAR",
-    "SUDARSH_KUNNAVAKKAM",
-    "THOMAS_BROADLEY",
-    "TIMOTHEE_CHAUVIN",
-    "TIMOTHY_KOKOTAJILO",
-]
-
 AI_TOOLS = ["NO_AI_TOOLS", "AI_TOOLS_AVAILABLE"]
 
-TERMINAL_GIFS = ["TERMINAL_GIFS"]
+TERMINAL_GIFS = ["TERMINAL_GIFS", "NO_TERMINAL_GIFS"]
 
 MANIFEST = {
     "settingsSchema": {
         "type": "object",
         "properties": {
-            "person": {"type": "string", "enum": PEOPLE + ["UNKNOWN_PERSON"]},
             "ai_tools": {
                 "type": "string",
                 "enum": AI_TOOLS + ["UNKNOWN_IF_AI_TOOLS_AVAILABLE"],
@@ -57,7 +19,7 @@ MANIFEST = {
             },
         },
         "additionalProperties": False,
-        "required": ["person", "ai_tools", "terminal_gifs"],
+        "required": ["ai_tools", "terminal_gifs"],
     },
     "stateSchema": {
         "type": "object",
@@ -70,29 +32,30 @@ MANIFEST = {
 
 def generate_manifest():
 
-    axis = [PEOPLE, AI_TOOLS, TERMINAL_GIFS]
+    axis = [AI_TOOLS, TERMINAL_GIFS]
     combinations = list(itertools.product(*axis))
 
     setting_packs = {}
-    for person, ai_tool, terminal_gif in combinations:
-        setting_pack_name = "-".join([person, ai_tool])
+    for ai_tool, terminal_gif in combinations:
+        terminal_gif_name = "" if terminal_gif == "NO_TERMINAL_GIFS" else terminal_gif
+        setting_pack_name = "-".join([ai_tool, terminal_gif_name])
+        if setting_pack_name[-1] == "-":
+            setting_pack_name = setting_pack_name[:-1]
         setting_packs[setting_pack_name] = {
-            "person": person,
             "ai_tools": ai_tool,
             "terminal_gifs": terminal_gif,
         }
 
     default_setting_pack = {
-        "person": "UNKNOWN_PERSON",
         "ai_tools": "UNKNOWN_IF_AI_TOOLS_AVAILABLE",
-        "terminal_gifs": "TERMINAL_GIFS",
+        "terminal_gifs": "NO_TERMINAL_GIFS",
     }
-    setting_packs["UNKNOWN_PERSON-UNKNOWN_IF_AI_TOOLS_AVAILABLE"] = default_setting_pack
+    setting_packs["UNKNOWN_IF_AI_TOOLS_AVAILABLE"] = (
+        default_setting_pack
+    )
 
     MANIFEST["settingsPacks"] = setting_packs
-    MANIFEST["defaultSettingsPack"] = (
-        "UNKNOWN_PERSON-UNKNOWN_IF_AI_TOOLS_AVAILABLE"
-    )
+    MANIFEST["defaultSettingsPack"] = "UNKNOWN_IF_AI_TOOLS_AVAILABLE"
     with open("manifest.json", "w") as f:
         f.write(json.dumps(MANIFEST, indent=4, sort_keys=True))
 
