cd593b63d6917273544ee7cca60c6a405a1ef96d
Sami Jawhar <sami@metr.org>
2024-09-17 18:56:33 +0000

    Save raw scoring output

--- src/score.py
+++ src/score.py
@@ -1,8 +1,15 @@
 import argparse
 import asyncio
+import enum
 import json
+import tempfile
 
 from src.settings import HOOKS, async_cleanup
+
+
+class ScoreAction(enum.Enum):
+    SCORE = "score"
+    LOG = "log"
 
 
 async def score():
@@ -10,7 +17,7 @@ async def score():
     if result.status == "invalidSubmission":
         print("Your submission is invalid. Please try again.")
     elif result.status == "processFailed":
-        print(f"Scoring failed. Please try again.")
+        print("Scoring failed. Please try again.")
     else:
         print(f"Score: {result.score}")
 
@@ -21,28 +28,46 @@ async def score():
     if result.message is not None:
         print(json.dumps(result.message, indent=2))
 
-    await async_cleanup()
+    return result.dict()
 
 
 async def log():
     score_log = await HOOKS.scoreLog()
     if not score_log:
         print("No score log found")
-    else:
-        for idx, entry in enumerate(score_log, start=1):
-            print(f"{idx} - {entry.elapsedSeconds} sec - {entry.score}")
-            for line in json.dumps(entry.message, indent=2).splitlines():
-                print(f"  {line}")
+        return []
+
+    for idx, entry in enumerate(score_log, start=1):
+        print(f"{idx} - {entry.elapsedSeconds} sec - {entry.score}")
+        for line in json.dumps(entry.message, indent=2).splitlines():
+            print(f"  {line}")
+
+    return [entry.dict() for entry in score_log]
+
+
+async def main(action: str | ScoreAction):
+    action = ScoreAction(action)
+    if action == ScoreAction.SCORE:
+        result = await score()
+    elif action == ScoreAction.LOG:
+        result = await log()
+
+    _, output_file = tempfile.mkstemp(suffix=".json")
+    with open(output_file, "w") as f:
+        json.dump(result, f)
+
+    print(f"Raw output saved to {output_file}")
 
     await async_cleanup()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("ACTION", type=str, choices=["score", "log"])
+    parser.add_argument(
+        "ACTION",
+        type=ScoreAction,
+        choices=[action.value for action in ScoreAction],
+    )
     args = parser.parse_args()
 
-    if args.ACTION == "score":
-        asyncio.run(score())
-    elif args.ACTION == "log":
-        asyncio.run(log())
+    asyncio.run(main(args.ACTION))
